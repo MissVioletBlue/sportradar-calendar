9e716626b5f142ae409125a459184069120ee6e1
rokudara-sen <matteo.habsburg@gmx.at>
2025-10-23 07:55:31 +0200

    Added Event entity and defined it

---
+++ Sportradar.Calendar.Domain/Entities/EntityEvent.cs
@@ -0,0 +1,22 @@
+﻿using Sportradar.Calendar.Domain.Enums;
+
+namespace Sportradar.Calendar.Domain.Entities;
+
+public sealed class EntityEvent
+{
+    public int Id { get; private set; }
+    public int SportId { get; private set; }
+    public DateTimeOffset StartsAt { get; private set; }
+    public string Title { get; private set; } = "Placeholder Title";
+    public EventStatus Status { get; private set; } = EventStatus.Scheduled;
+    
+    public EntityEvent(int id, int sportId, DateTimeOffset startsAt, string title)
+    {
+        Id = id;
+        SportId = sportId;
+        StartsAt = startsAt;
+        Title = title; ;
+    }
+    
+    private EntityEvent() {} // TODO: IMPLEMENT EF LATER.
+}
