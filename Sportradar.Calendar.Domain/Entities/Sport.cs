11a734d628a6e6f6059acb478214721f3374e583
rokudara-sen <if23b266@technikum-wien.at>
2025-11-05 14:09:03 +0100

    Added sport entity

---
+++ Sportradar.Calendar.Domain/Entities/Sport.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sportradar.Calendar.Domain.Entities;
+
+public sealed class Sport
+{
+    [Key]
+    public int Id { get; private set; }
+
+    [Required]
+    [StringLength(60)]
+    public string Name { get; private set; }
+
+    public Sport(int id, string name)
+    {
+        Id = id;
+        Name = name.Trim();
+    }
+
+    public Sport(string name)
+    {
+        Name = name.Trim();
+    }
+
+    private Sport()
+    {
+        Name = string.Empty;
+    }
+}
